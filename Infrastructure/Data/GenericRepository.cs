680b2068184129e275c915694f145953a88b8c69
--global <--global>
2022-06-13 21:11:48 +0400

    Left Overs

---
+++ Infrastructure/Data/GenericRepository.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+using Core.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+        private readonly StoreContext context;
+
+        public GenericRepository(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+           return await context.Set<T>().ToListAsync();
+        }
+
+       
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<List<T>> GetAllAsyncWithSpec(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+
+        public async Task<T> GetEntityAsyncWithSpec(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).FirstOrDefaultAsync();
+        }
+
+         private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(context.Set<T>().AsQueryable(),spec);
+        }
+    }
+}
---
+++ Infrastructure/Data/IGenericRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Specifications;
+
+namespace Infrastructure.Data
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> GetAllAsyncWithSpec(ISpecification<T> spec);
+        Task<T> GetEntityAsyncWithSpec(ISpecification<T> spec);
+    }
+}
